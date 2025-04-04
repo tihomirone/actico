3b184806af9c9978d1192715652dcec0adda598d
tihomir <tihomir17@yahoo.com>
2025-04-04 19:26:52 +0300

    Initial setup.

---
+++ src/main/java/com/example/actico/ActicoApplication.java
@@ -0,0 +1,13 @@
+package com.example.actico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/actico/ActicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.actico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
